c69b280a811bc0bf01affdb7dd2103e9244cb9c4
Tonye Jack <jtonye@ymail.com>
2022-10-30 00:56:44 -0600

    Update action

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ if [[ "$INPUT_LICENSE" == "true" ]]; then
 fi
 
 if [[ -n "$INPUT_GRAPHVIZ_DIR" ]]; then
-  ARGS="$ARGS --graphviz-dir=$INPUT_GRAPHVIZ_DIR"
+  ARGS="$ARGS --graphviz-dir $INPUT_GRAPHVIZ_DIR"
 fi
 
 if [[ "$INPUT_LOW_QUALITY" == "true" ]]; then
@@ -21,11 +21,11 @@ if [[ "$INPUT_ANOMALIES_LENGTH_CHANGE" == "true" ]]; then
 fi
 
 if [[ -n "$INPUT_ANOMALIES_PAGE_LENGTH" ]]; then
-  ARGS="$ARGS --anomaliesPageLength=$INPUT_ANOMALIES_PAGE_LENGTH"
+  ARGS="$ARGS --anomaliesPageLength $INPUT_ANOMALIES_PAGE_LENGTH"
 fi
 
 if [[ -n "$INPUT_CATALOG" ]]; then
-  ARGS="$ARGS --catalog=$INPUT_CATALOG"
+  ARGS="$ARGS --catalog $INPUT_CATALOG"
 fi
 
 if [[ "$INPUT_CHECK_LENGTH_CHANGE" == "true" ]]; then
@@ -33,23 +33,23 @@ if [[ "$INPUT_CHECK_LENGTH_CHANGE" == "true" ]]; then
 fi
 
 if [[ -n "$INPUT_CHECK_PAGE_LENGTH" ]]; then
-  ARGS="$ARGS --checkPageLength=$INPUT_CHECK_PAGE_LENGTH"
+  ARGS="$ARGS --checkPageLength $INPUT_CHECK_PAGE_LENGTH"
 fi
 
 if [[ -n "$INPUT_COLUMN_LENGTH_CHANGE" ]]; then
-  ARGS="$ARGS --columnLengthChange=$INPUT_COLUMN_LENGTH_CHANGE"
+  ARGS="$ARGS --columnLengthChange $INPUT_COLUMN_LENGTH_CHANGE"
 fi
 
 if [[ -n "$INPUT_COLUMN_PAGE_LENGTH" ]]; then
-  ARGS="$ARGS --columnPageLength=$INPUT_COLUMN_PAGE_LENGTH"
+  ARGS="$ARGS --columnPageLength $INPUT_COLUMN_PAGE_LENGTH"
 fi
 
 if [[ -n "$INPUT_DATABASE_NAME" ]]; then
-  ARGS="$ARGS --database-name=$INPUT_DATABASE_NAME"
+  ARGS="$ARGS --database-name $INPUT_DATABASE_NAME"
 fi
 
 if [[ -n "$INPUT_DATABASE_TYPE" ]]; then
-  ARGS="$ARGS --database-type=$INPUT_DATABASE_TYPE"
+  ARGS="$ARGS --database-type $INPUT_DATABASE_TYPE"
 fi
 
 if [[ "$INPUT_DB_OBJECT_LENGTH_CHANGE" == "true" ]]; then
@@ -57,7 +57,7 @@ if [[ "$INPUT_DB_OBJECT_LENGTH_CHANGE" == "true" ]]; then
 fi
 
 if [[ -n "$INPUT_DB_OBJECT_PAGE_LENGTH" ]]; then
-  ARGS="$ARGS --dbObjectPageLength=$INPUT_DB_OBJECT_PAGE_LENGTH"
+  ARGS="$ARGS --dbObjectPageLength $INPUT_DB_OBJECT_PAGE_LENGTH"
 fi
 
 if [[ "$INPUT_DEBUG" == "true" ]]; then
@@ -65,11 +65,11 @@ if [[ "$INPUT_DEBUG" == "true" ]]; then
 fi
 
 if [[ -n "$INPUT_DEGREE" ]]; then
-  ARGS="$ARGS -degree=$INPUT_DEGREE"
+  ARGS="$ARGS -degree $INPUT_DEGREE"
 fi
 
 if [[ -n "$INPUT_DRIVER_PATH" ]]; then
-  ARGS="$ARGS --driverPath=$INPUT_DRIVER_PATH"
+  ARGS="$ARGS --driverPath $INPUT_DRIVER_PATH"
 fi
 
 if [[ "$INPUT_FK_LENGTH_CHANGE" == "true" ]]; then
@@ -77,15 +77,15 @@ if [[ "$INPUT_FK_LENGTH_CHANGE" == "true" ]]; then
 fi
 
 if [[ -n "$INPUT_FK_PAGE_LENGTH" ]]; then
-  ARGS="$ARGS --fkPageLength=$INPUT_FK_PAGE_LENGTH"
+  ARGS="$ARGS --fkPageLength $INPUT_FK_PAGE_LENGTH"
 fi
 
 if [[ -n "$INPUT_HOST" ]]; then
-  ARGS="$ARGS --host=$INPUT_HOST"
+  ARGS="$ARGS --host $INPUT_HOST"
 fi
 
 if [[ -n "$INPUT_IMAGE_FORMAT" ]]; then
-  ARGS="$ARGS --image-format=$INPUT_IMAGE_FORMAT"
+  ARGS="$ARGS --image-format $INPUT_IMAGE_FORMAT"
 fi
 
 if [[ "$INPUT_INDEX_LENGTH_CHANGE" == "true" ]]; then
@@ -93,11 +93,11 @@ if [[ "$INPUT_INDEX_LENGTH_CHANGE" == "true" ]]; then
 fi
 
 if [[ -n "$INPUT_INDEX_PAGE_LENGTH" ]]; then
-  ARGS="$ARGS --indexPageLength=$INPUT_INDEX_PAGE_LENGTH"
+  ARGS="$ARGS --indexPageLength $INPUT_INDEX_PAGE_LENGTH"
 fi
 
 if [[ -n "$INPUT_SCHEMA_META" ]]; then
-  ARGS="$ARGS --schema-meta=$INPUT_SCHEMA_META"
+  ARGS="$ARGS --schema-meta $INPUT_SCHEMA_META"
 fi
 
 if [[ "$INPUT_NO_ANOMALIES_PAGING" == "true" ]]; then
@@ -141,15 +141,15 @@ if [[ "$INPUT_NO_IMPLIED" == "true" ]]; then
 fi
 
 if [[ -n "$INPUT_OUTPUT_DIR" ]]; then
-  ARGS="$ARGS --outputDirectory=$INPUT_OUTPUT_DIR"
+  ARGS="$ARGS -o $INPUT_OUTPUT_DIR"
 fi
 
 if [[ -n "$INPUT_PORT" ]]; then
-  ARGS="$ARGS --port=$INPUT_PORT"
+  ARGS="$ARGS --port $INPUT_PORT"
 fi
 
 if [[ -n "$INPUT_RENDERER" ]]; then
-  ARGS="$ARGS --renderer=$INPUT_RENDERER"
+  ARGS="$ARGS --renderer $INPUT_RENDERER"
 fi
 
 if [[ "$INPUT_ROUTINE_LENGTH_CHANGE" == "true" ]]; then
@@ -157,11 +157,11 @@ if [[ "$INPUT_ROUTINE_LENGTH_CHANGE" == "true" ]]; then
 fi
 
 if [[ -n "$INPUT_ROUTINE_PAGE_LENGTH" ]]; then
-  ARGS="$ARGS --routinePageLength=$INPUT_ROUTINE_PAGE_LENGTH"
+  ARGS="$ARGS --routinePageLength $INPUT_ROUTINE_PAGE_LENGTH"
 fi
 
 if [[ -n "$INPUT_SCHEMA" ]]; then
-  ARGS="$ARGS --schema=$INPUT_SCHEMA"
+  ARGS="$ARGS --schema $INPUT_SCHEMA"
 fi
 
 if [[ "$INPUT_SSO" == "true" ]]; then
@@ -173,11 +173,11 @@ if [[ "$INPUT_TABLE_LENGTH_CHANGE" == "true" ]]; then
 fi
 
 if [[ -n "$INPUT_TABLE_PAGE_LENGTH" ]]; then
-  ARGS="$ARGS --tablePageLength=$INPUT_TABLE_PAGE_LENGTH"
+  ARGS="$ARGS --tablePageLength $INPUT_TABLE_PAGE_LENGTH"
 fi
 
 if [[ -n "$INPUT_USER" ]]; then
-  ARGS="$ARGS --user=$INPUT_USER"
+  ARGS="$ARGS --user $INPUT_USER"
 fi
 
 if [[ "$INPUT_VIZJS" == "true" ]]; then
